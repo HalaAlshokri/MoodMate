c93452ee79912f015deff510600df191d98e19c7
HalaAlshokri <119620071+HalaAlshokri@users.noreply.github.com>
2024-05-26 01:34:24 +0300

    Tabs changes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -15,20 +15,38 @@ export default function TabLayout() {
         headerShown: false,
       }}>
       <Tabs.Screen
-        name="index"
+        name="Calendar"
         options={{
-          title: 'Home',
+          title: 'Calendar',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+            <TabBarIcon name={focused ? 'calendar-sharp' : 'calendar-outline'} color={color} />
           ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="Timeline"
         options={{
-          title: 'Explore',
+          title: 'Timeline',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon name={'search'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Analytics"
+        options={{
+          title: 'Analytics',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'analytics' : 'analytics-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'settings-sharp' : 'settings-outline'} color={color} />
           ),
         }}
       />
